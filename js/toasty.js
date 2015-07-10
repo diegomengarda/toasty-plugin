65a91feead89a707332baca594e33f739987d905
unknown <diegormengarda@gmail.com>
2015-07-10 15:46:38 -0300

    Type and Click Working

--- js/toasty.js
+++ js/toasty.js
@@ -1,62 +1,71 @@
-;(function (factory) {
-	'use strict';
-	if (typeof define === 'function' && define.amd) {
-		define(['jquery'], factory);
-	} else {
-		factory(jQuery);
-	}
-}(function ($) {
-	'use strict';
+(function($) {
+
+	var toasty = {};
 	
 	var defaults = {
 		sound 		: true,
- 		image 		: 'toasty.png',
  		sound 		: 'toasty.mp3',
+ 		image 		: 'toasty.png',
  		type 		: 'default', // word or default
  		typeWord 	: 'toasty'
 	};
-	
-    $.fn.toasty = function(options) {
-		
-		var countInstancias = this.length;
-		if(countInstancias == 0) return this;
-		if(countInstancias > 1){
-			this.each(function(){$(this).toasty(options)});
-			return this;
-		}
 
-		var toasty 		= {};
-		toasty.el  		= this;
-		toasty.settings = $.extend({}, defaults, options);
-		
-		var init = function(){
-			$("body").append('<div id="toasty-box" style="position: fixed;right:-200px;bottom:0px;"><img src="'+ toasty.settings.image +'" alt="toasty"></div>');
-			if(toasty.settings.sound)
-				$("body").append('<audio id="toasty-audio"><source src="'+ toasty.settings.sound +'" type="audio/mpeg"></source></audio>');
-		}
-		
-		var show = function() {			
-			var audio = document.getElementById('toasty-audio');
-			audio.play();
-			$("#toasty-box").addClass("show-surprise");
-			//setTimeout( function(){ $("#toasty-guy-dan").removeClass("show-dan"); }, 1000);          
-		}
+	var commands = {
+		show: show
+	};
+
+	$.fn.toasty = function() {
+        if (typeof arguments[0] === 'string') {
+            var property = arguments[1];
+            var args = Array.prototype.slice.call(arguments);
+            args.splice(0, 1);
+            createToasty(args);
+            commands[arguments[0]].apply(this, args);
+        }
+        else {
+            createToasty.apply(this, arguments);
+        }
+        return this;
+    };
+
+    function createToasty(options) {
+    	toasty.settings = $.extend({}, defaults, options);
+    	init();
+    };
+
+    function init(){
+		$("body").append('<div id="toasty-box" style="position:fixed;right:-200px;bottom:0px;"><img src="'+ toasty.settings.image +'" alt="toasty"></div>');
+		if(toasty.settings.sound)
+			$("body").append('<audio id="toasty-audio"><source src="'+ toasty.settings.sound +'" type="audio/mpeg"></source></audio>');
 
 		if(toasty.settings.type == 'word') {
+			var typingTimer;
+			var doneTypingInterval = 800;
 			var typedWord = '';
+			$('body').on('keyup', function(){
+			    clearTimeout(typingTimer);
+			    typingTimer = setTimeout(doneTyping, doneTypingInterval);
+			});
 			$('body').on('keypress', function(e) {
 				var c = String.fromCharCode(e.charCode != null ? e.charCode : e.keyCode);
 				typedWord += c.toLowerCase();
-				if(typedWord.length > 4) typedWord = typedWord.slice(1);
 				if(typedWord == toasty.settings.typeWord) {
-					toasty.el.show();
+					show();
 				};
 			});
+			$('body').on('keydown', function(){
+			    clearTimeout(typingTimer);
+			});
+			var doneTyping = function() {
+			    typedWord = '';
+			}
 		}
-		
-		init();
-				
-		return toasty.el;
-    };
+	};
 	
-}));
+	function show() {
+		var audio = $('#toasty-audio')[0].play();
+		$("#toasty-box").addClass("show-surprise");
+		setTimeout( function(){ $("#toasty-box").removeClass("show-surprise"); }, 1000);
+	};
+	
+})(jQuery);
